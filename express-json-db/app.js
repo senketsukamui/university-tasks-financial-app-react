3f35280c6594b7ed087913c182e73c1c9add0ab4
gleb.potapov.0138@gmail.com <glebpotapov2001@gmail.com>
2019-12-21 20:16:21 +0300

    Fix requests

--- express-json-db/app.js
+++ express-json-db/app.js
@@ -7,21 +7,30 @@ const adapter = new FileSync("db.json");
 const db = low(adapter);
 
 const app = express();
-
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 // app.get("/", (req, res) => {
 //   res.send("Hello world!");
 // });
+db.defaults({ data: {} }).write();
 
 app.post("/api/post_category", jsonParser, (req, res) => {
+  console.log("check");
+  console.log(req.body);
   let { title } = req.body;
-  db.get()
+  db.get("data")
     .push({ title: {} })
     .write();
+  res.status(200).json({});
 });
 
-app.post("/api/post_finance", jsonParser, (req, res) => {
+app.post("/api/post_finance", (req, res) => {
   let { title, price, category } = req.body;
-  db.get(category)
+  db.get("data")
+    .get("category")
     .push({ title: title, price: price })
     .write();
 });
