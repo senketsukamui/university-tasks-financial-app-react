6ce6522ad95d5c23103c6d045ad51fbb0a290c50
gleb.potapov.0138@gmail.com <glebpotapov2001@gmail.com>
2020-01-19 20:07:47 +0300

    Make category chart

--- client/src/components/Analytics/index.jsx
+++ client/src/components/Analytics/index.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import "./index.scss";
 import { connect } from "react-redux";
 import _ from "lodash";
-import { Label, Input } from "reactstrap";
+import { Label, Input, Form, FormGroup } from "reactstrap";
+import {
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  BarChart,
+  Bar,
+  PieChart,
+  Pie,
+  Sector,
+  Cell,
+  LineChart,
+  Line
+} from "recharts";
 const Analytics = props => {
   const categoriesList = Object.keys(props.categories);
   const options = categoriesList.map((p, i) => {
@@ -12,20 +27,61 @@ const Analytics = props => {
   const onSelectChange = event => {
     setFormState(event.target.value);
   };
-  console.log("form", formState);
+  const getCategoryData = inputCategory => {
+    return _.get(props.categories, inputCategory, []);
+  };
+  const createCategoryChart = inputCategory => {
+    let chartData = getCategoryData(inputCategory);
+    console.log(chartData);
+    return (
+      <LineChart
+        width={500}
+        height={300}
+        data={chartData}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="price"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+      </LineChart>
+    );
+  };
   return (
     <div className="analytics__wrapper">
       <div className="analytics__title">Categorical analytics</div>
       <div className="analytics__select-menu">
-        <Label>Select Category</Label>
-        <Input
-          type="select"
-          onChange={onSelectChange}
-          value=""
-          className="analytics__select-menu__select"
-        >
-          {options}
-        </Input>
+        <Form>
+          <FormGroup>
+            <Label>Select Category</Label>
+            <Input
+              type="select"
+              onChange={onSelectChange}
+              defaultValue=""
+              className="analytics__select-menu__select"
+            >
+              <option selected disabled hidden>
+                click here to choose category
+              </option>
+              {options}
+            </Input>
+          </FormGroup>
+        </Form>
+        <div className="analytics__category-chart">
+          {createCategoryChart(formState)}
+        </div>
       </div>
     </div>
   );
